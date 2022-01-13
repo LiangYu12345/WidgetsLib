03a3ea42bfa92e78054a4e03564f8d503d8d3064
li.zhao <netzhaoli@163.com>
2022-01-13 12:18:49 +0800

    添加DragButton

---
+++ Button/dragbutton.cpp
@@ -0,0 +1,38 @@
+﻿#include "dragbutton.h"
+#include <QMouseEvent>
+#include <QApplication>
+#include <QDrag>
+#include <QMimeData>
+
+DragButton::DragButton(QWidget *parent) : QPushButton{parent}
+{
+
+}
+
+DragButton::DragButton(const QString &text, QWidget *parent) : QPushButton(text, parent)
+{
+
+}
+
+DragButton::DragButton(const QIcon &icon, const QString &text, QWidget *parent) : QPushButton(icon, text, parent)
+{
+
+}
+
+void DragButton::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+        m_dragStartPosition = event->pos();
+}
+
+void DragButton::mouseMoveEvent(QMouseEvent *event)
+{
+    if (!(event->buttons() & Qt::LeftButton) ||
+            (event->pos() - m_dragStartPosition).manhattanLength() < QApplication::startDragDistance())
+        return;
+    QDrag *drag = new QDrag(this);
+    QMimeData *mimeData = new QMimeData;
+    drag->setMimeData(mimeData);
+
+    Qt::DropAction dropAction = drag->exec(Qt::CopyAction | Qt::MoveAction);
+}
---
+++ Button/dragbutton.h
@@ -0,0 +1,24 @@
+﻿#ifndef DRAGBUTTON_H
+#define DRAGBUTTON_H
+
+#include <QPushButton>
+
+class DragButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    explicit DragButton(QWidget *parent = nullptr);
+    explicit DragButton(const QString &text, QWidget *parent = nullptr);
+    explicit DragButton(const QIcon &icon, const QString &text, QWidget *parent = nullptr);
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+
+
+private:
+    QPoint m_dragStartPosition;
+
+};
+
+#endif // DRAGBUTTON_H
